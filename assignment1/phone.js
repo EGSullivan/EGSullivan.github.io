59daefbffd7124a108293ec3b3c929d60d67f904
EGSullivan <36346052+EGSullivan@users.noreply.github.com>
2019-01-30 08:25:07 -0700

    Update phone.js

--- assignment1/phone.js
+++ assignment1/phone.js
@@ -1,5 +1,5 @@
 $(document).ready(function() { // do this when the document is loaded
-	$("#tBar").show(); // show tab bar
+	$("#tBar").show() // show tab bar
 	$("#tabCon1").show(); // show dialer
 	$("#tabCon2").hide(); // hide contact list
 	$("#tabCon3").hide(); // hide add contact
@@ -14,9 +14,9 @@ $("#tab1").click(function() { // when "tab1" is clicked
 
 $("#tab2").click(function() { // when "tab2" is clicked
 	$("#tBar").show(); // show tab bar
-	$("#tabCon1").hide(): // hide dialer
-	$("#tabCon2").show(): // show contact list
-	$("#tabCon3").hide(): // hide add contact
+	$("#tabCon1").hide(); // hide dialer
+	$("#tabCon2").show(); // show contact list
+	$("#tabCon3").hide(); // hide add contact
 });
 
 $("#tab3").click(function() { // when "tab3" is clicked
