c3b25436b90605063dfafe8611ee7d08d8c736f7
EGSullivan <36346052+EGSullivan@users.noreply.github.com>
2019-02-12 20:09:49 -0700

    Update phone.js

--- assignment2/phone.js
+++ assignment2/phone.js
@@ -10,6 +10,7 @@ $("#tab1").click(function() { // when "tab1" is clicked
 	$("#tabCon1").show();	// show dialer
 	$("#tabCon2").hide(); // hide contact list
 	$("#tabCon3").hide(); // hide add contact
+	$("#tabCon4").hide(); // hide help page
 });
 
 $("#tab2").click(function() { // when "tab2" is clicked
@@ -17,6 +18,7 @@ $("#tab2").click(function() { // when "tab2" is clicked
 	$("#tabCon1").hide(); // hide dialer
 	$("#tabCon2").show(); // show contact list
 	$("#tabCon3").hide(); // hide add contact
+	$("tabCon4").hide(); // hide help page
 });
 
 $("#tab3").click(function() { // when "tab3" is clicked
@@ -24,4 +26,13 @@ $("#tab3").click(function() { // when "tab3" is clicked
 	$("#tabCon1").hide(); // hide dialer
 	$("#tabCon2").hide(); // hide contact list
 	$("#tabCon3").show(); // show add contact
+	$("#tabCon4").hide(); // hide help page
+});
+
+$("#tab4").click(function() { //when "tab4" is clicked
+	$("tBar").show(); // show tab bar
+	$("#tabCon1").hide(); // hide dialer
+	$("tabCon2").hide(); // hide contact list
+	$("tabCon4").show(); // show help page
+	
 });
