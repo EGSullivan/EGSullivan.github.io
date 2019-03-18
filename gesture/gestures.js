7cceebfe031c9c9101a6271143669804f23e9e0b
EGSullivan <36346052+EGSullivan@users.noreply.github.com>
2019-03-18 15:43:06 -0600

    Update gestures.js

--- gesture/gestures.js
+++ gesture/gestures.js
@@ -1,1 +1,23 @@
-/* This file intentionally left blank. */
+downX = 0
+downY = 0
+
+$("#gestureArea").mousedown(function(event) {
+	$("#gestureResult").text("mouse down");
+	var downX = (event.pageX)
+	var downY = (event.pageY)
+})
+	
+	
+
+$("#gestureArea").mouseup(function(event) {
+	
+	var upX = (event.pageX)
+	var upY = (event.pageY)
+	
+	if upX < downX:
+		$("#gestureResult").text("swipe left")
+	else if upX > downX:
+		$("#gestureResult").text("swipe right")
+	else if upX == downX:
+		$("#gestureResult").text("mouse up");
+})
